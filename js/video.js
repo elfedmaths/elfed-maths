531739685a9b4b31a0509f309bfb49d7fa9290f3
mattymassey95 <114009907+elfedmaths@users.noreply.github.com>
2022-09-22 20:30:56 +0100

    Update video.js

--- js/video.js
+++ js/video.js
@@ -2,7 +2,16 @@ var video = document.querySelectorAll('video');
 
 video.forEach(play => play.addEventListener('click', () => {
 
-    if(play.classList.contains('active')){
+    var clicked = play.classList.contains('active');
+
+    video.forEach(box => {
+        box.classList.remove('active');
+        box.controls = false;
+        box.currentTime = 1;
+        box.pause();
+    })
+
+    if(clicked){
         play.classList.remove('active');
         play.controls = false;
         play.currentTime = 1;
