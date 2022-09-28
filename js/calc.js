c73c40d9d3a0dc3ba109d210431f871c7c6e5b7a
mattymassey95 <114009907+elfedmaths@users.noreply.github.com>
2022-09-28 19:31:10 +0100

    New questions added

--- js/calc.js
+++ js/calc.js
@@ -91,19 +91,21 @@ function getCalc(type){
             break;
         /* -------------------- Percentages -------------------- */
         case 'perc-find-1':
-            var int1 = randomInt(0, 2);
-            var perc = [2, 10, 100];
-            var phrase = ["50%", "10%", "1%"];
-            var min = 10;
-            var max = 1000;
+            var int1 = randomInt(0, 3);
+            var perc = [2, 4, 10, 100];
+            var phrase = ["50%", "25%", "10%", "1%"];
+            var min = 10, max = 1000;
             switch (int1){
                 case 0: 
                     var int2 = randomInt((min / 2), (max / 2)) * 2;
                     break;
                 case 1:
-                    var int2 = randomInt((min / 10), (max / 10)) * 10;
+                    var int2 = randomInt((min / 4), (max / 4)) * 4;
                     break;
                 case 2:
+                    var int2 = randomInt((min / 10), (max / 10)) * 10;
+                    break;
+                case 3:
                     var int2 = randomInt((min / 100), (max / 100)) * 100;
                     break;
                 default:
@@ -154,13 +156,13 @@ function getCalc(type){
             var perc = randomInt(1, 99);
             var mult = perc / 100;
             if(int1 == 1){
-                var quest = "Decrease by " + perc + "%";
+                var quest = "What multiplier would you use to<br>Decrease by " + perc + "%";
                 var ans = (1 - mult).toFixed(2);
             }else if(int1 == 2){
-                var quest = "Increase by " + perc + "%";
+                var quest = "What multiplier would you use to<br>Increase by " + perc + "%";
                 var ans = (1 + mult).toFixed(2);
             }else{
-                var quest = "Find " + perc + "%";
+                var quest = "What multiplier would you use to<br>Find " + perc + "%";
                 var ans = mult;
             }
             break;
@@ -273,22 +275,150 @@ function getCalc(type){
             var ans = "<sup>" + ans1['n'] + "</sup>&frasl;<sub>" + ans1['d'] + "</sub>";
             break;
         case 'frac-amount':
-            var quest = "";
-            var ans = "";
+            var den1 = randomInt(2,15);
+            var int1 = randomInt(2,10);
+            var num1 = randomInt(1, den1-1 );
+            var int2 = den1 * int1;
+            var quest = "<sup>" + num1 + "</sup>&frasl;<sub>" + den1 + "</sub> of " + int2;
+            var ans = (int2 / den1) * num1;
             break;
         /* -------------------- Order of Operations -------------------- */
-        case 'bodmas':
-            var quest = "";
-            var ans = "";
+        case 'ooo-add-sub':
+            var int1 = randomInt(1,20);
+            var int2 = randomInt(1,20);
+            var int3 = randomInt(1,20);
+            var form1 = [                
+                [`${int1} + ${int2} - ${int3}`, (int1 + int2) - int3],
+                [`${int1} - ${int2} + ${int3}`, (int1 - int2) + int3],
+                [`(${int1} + ${int2}) - ${int3}`, (int1 + int2) - int3],
+                [`(${int1} - ${int2}) + ${int3}`, (int1 - int2) + int3],
+                [`${int1} + (${int2} - ${int3})`, int1 + (int2 - int3)],
+                [`${int1} - (${int2} + ${int3})`, int1 - (int2 + int3)]
+            ];
+            var num = randomInt(0, form1.length);
+            var quest = form1[num][0];
+            var ans = form1[num][1];
+            break;
+        case 'ooo-mult-div':
+            var int1 = randomInt(1,10);
+            var int2 = randomInt(1,10);
+            var int3 = randomInt(1,10);
+            var int4 = randomInt(2,5) * int1;
+            var int5 = randomInt(2,5) * int2 * int3;
+            var form1 = [                
+                [`${int4} x ${int2} &divide; ${int1}`, (int4 * int2) / int1],
+                [`${int4} &divide; ${int1} x ${int3}`, (int4 / int1) * int3],
+                [`(${int4} x ${int2}) &divide; ${int1}`, (int4 * int2) / int1],
+                [`(${int4} &divide; ${int1}) x ${int2}`, (int4 / int1) * int2],
+                [`${int2} x (${int4} &divide; ${int1})`, int2 * (int4 / int1)],
+                [`${int5} &divide; (${int2} x ${int3})`, int5 / (int2 * int3)]
+            ];
+            var num = randomInt(0, form1.length);
+            var quest = form1[num][0];
+            var ans = form1[num][1];
             break;
         /* -------------------- Algebra -------------------- */
         case 'alg-simp-1':
+            var letters = "abcdefghjkmnpqrstuvwxyz";
+            var letter = letters.charAt(randomInt(0, letters.length-1));
+            var int1 = randomInt(2,5);
+            var int2 = 0, ans = 0;
             var quest = "";
-            var ans = "";
+            for (let i = 0; i < int1; i++) {
+                int2 = randomNeg(1, 10);
+                if(int2 == 1){ 
+                    quest += " +" + letter;
+                    ans += 1;
+                }else if(int2 == -1){ 
+                    quest += " -" + letter;
+                    ans += -1;
+                }else if(int2 > 0){
+                    quest += " +" + int2 + letter;
+                    ans += int2;
+                }else{
+                    quest += " " + int2 + letter;
+                    ans += int2;
+                }                             
+            }
+            if(quest.charAt(1) == "+"){
+                quest = quest.slice(2);
+            }
+            ans += letter;   
             break;
         case 'alg-simp-2':
+            var letters = "abcdefghjkmnpqrstuvwxyz";
+            var letter1 = letters.charAt(randomInt(0, letters.length-1));
+            letters = letters.replace(letter1, "");
+            var letter2 = letters.charAt(randomInt(0, letters.length-1));
+            var int1 = randomInt(3,5);
+            var int2 = 0, ans1 = 0, ans2 = 0;
             var quest = "";
-            var ans = "";
+            for (let i = 0; i < int1; i++) {
+                int2 = randomNeg(1, 10);
+                int3 = randomNeg(1, 2);
+                if(int2 == 1){ 
+                    if(int3 == 1){
+                        quest += " +" + letter1;
+                        ans1 += 1;
+                    }else{
+                        quest += " +" + letter2;
+                        ans2 += 1;
+                    }
+                }else if(int2 == -1){ 
+                    if(int3 == 1){
+                        quest += " -" + letter1;
+                        ans1 += -1;
+                    }else{
+                        quest += " -" + letter2;
+                        ans2 += -1;
+                    }
+                }else if(int2 > 0){
+                    if(int3 == 1){
+                        quest += " +" + int2 + letter1;
+                        ans1 += int2;
+                    }else{
+                        quest += " +" + int2 + letter2;
+                        ans2 += int2;
+                    }
+                }else{
+                    if(int3 == 1){
+                        quest += " " + int2 + letter1;
+                        ans1 += int2;
+                    }else{
+                        quest += " " + int2 + letter2;
+                        ans2 += int2;
+                    }
+                }                             
+            }
+            if(quest.charAt(1) == "+"){
+                quest = quest.slice(2);
+            }
+            if(ans1 == 0){
+                ans1 = "";
+            }else if(ans1 == 1){
+                ans1 = letter1;
+            }else if(ans1 == -1){
+                ans1 = "-" + letter1;
+            }else if(ans1 > 0){
+                ans1 = " +" + ans1 + letter1;
+            }else if(ans1 < 0){
+                ans1 = " " + ans1 + letter1;
+            }
+            if(ans2 == 0){
+                ans2 = "";
+            }else if(ans2 == 1){
+                ans2 = " +" + letter2;
+            }else if(ans2 == -1){
+                ans2 = " -" + letter2;
+            }else if(ans2 > 0){
+                ans2 = " +" + ans2 + letter2;
+            }else if(ans2 < 0){
+                ans2 = " " + ans2 + letter2;
+            }
+            var ans = ans1 + ans2;
+            if(ans.charAt(1) == "+"){
+                ans = ans.slice(2);
+            }   
             break;
         case 'alg-sub':
             var quest = "";
