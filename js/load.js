c0f77f32b4c414f7c4c3dc7572ed89907866cad3
mattymassey95 <114009907+elfedmaths@users.noreply.github.com>
2023-01-14 18:58:55 +0000

    January Update

---
+++ js/load.js
@@ -0,0 +1,21 @@
+function loadHtml(filename, datatype){
+    let xhttp;
+    let element = document.getElementById('template');
+    let file = filename;
+    if(file){
+        xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function(){
+            if(this.readyState == 4){
+                if(this.status == 200){
+                    element.innerHTML = this.responseText;
+                }
+                if(this.status == 400){
+                    element.innerHTML = "<h1>Page Not Found</h1>";
+                }
+            }
+        }
+        xhttp.open("GET", `./templates/${file}.html`, true);
+        xhttp.send();
+        return;
+    }
+}
