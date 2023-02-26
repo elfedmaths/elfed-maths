09f1f465f622cd1dcb58ab2ce8ca5a12d9f9cad7
mattymassey95 <114009907+elfedmaths@users.noreply.github.com>
2023-02-26 20:12:20 +0000

    Update popup.activities.js

--- js/popup.activities.js
+++ js/popup.activities.js
@@ -2,18 +2,23 @@
 
 const contentBoxes = document.querySelectorAll(".content-box");
 contentBoxes.forEach((contentBox) => {
-  contentBox.addEventListener("click", function() {
-    var contentBack = contentBox.querySelector(".background");
-    var contentText = contentBox.querySelector(".content");
-    if (contentBack.classList.contains("hidden")) {
-      contentBack.classList.remove("hidden");
-      contentText.classList.remove("hidden");
-      textFit(contentText.querySelectorAll('h3'));
-      textFit(contentText.querySelectorAll('p'),{multiline:true});
-      textFit(contentText.querySelectorAll('.stopwatch p'),{multiline:true});
-    } else {
-      contentBack.classList.add("hidden");
-      contentText.classList.add("hidden");
-    }
-  });
+  if(contentBox.id != "fixed-content"){
+    contentBox.addEventListener("click", function() {
+      var contentBack = contentBox.querySelector(".background");
+      var contentText = contentBox.querySelector(".content");
+      var contentTitle = contentBox.querySelector("h2");
+      if (contentBack.classList.contains("hidden")) {
+        contentBack.classList.remove("hidden");
+        contentText.classList.remove("hidden");
+        contentTitle.classList.add("hidden-mobile");
+        textFit(contentText.querySelectorAll('h3'));
+        textFit(contentText.querySelectorAll('p'),{multiline:true});
+        textFit(contentText.querySelectorAll('.stopwatch p'),{multiline:true});
+      } else {
+        contentBack.classList.add("hidden");
+        contentText.classList.add("hidden");
+        contentTitle.classList.remove("hidden-mobile");
+      }
+    });
+  }
 });
